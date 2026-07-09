36e6e68fab36b13edab3bbdb7234525352ea6e1d
gda00 <gabrieldutraamaral@gmail.com>
2026-07-08 21:48:00 -0300

    fix: css and js checks

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,11 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const triggers = document.querySelectorAll(".dropdown-trigger");
-    
-    triggers.forEach(trigger => {
-        const content = trigger.nextElementSibling;
+/*global document */
+document.addEventListener("DOMContentLoaded", function () {
+    "use strict";
+    var triggers = document.querySelectorAll(".dropdown-trigger");
+    triggers.forEach(function (trigger) {
+        var content = trigger.nextElementSibling;
         content.style.display = "none";
-        
-        trigger.addEventListener("click", () => {
+        trigger.addEventListener("click", function () {
             if (content.style.display === "none") {
                 content.style.display = "block";
             } else {
